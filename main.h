249a9d136df85531b93e43b0a6510e8adf145bb5
ikerikks <ikerikks@gmail.com>
2022-07-06 13:15:36 +0200

    update of header file

--- main.h
+++ main.h
@@ -1,6 +1,16 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <stdio.h>
+#include <stdarg.h>
+
+typedef struct print
+{
+	char *s;
+	int (*i)(va_list);
+
+} print_f;
+
 /* Prototypes */
 
 int _printf(const char *format, ...);
