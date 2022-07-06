b2d9b822e3c0db623ea69d831be87b53d7cdbf6e
Mab <mac@128-75-0-112.broadband.corbina.ru>
2022-07-06 14:35:58 +0200

    Update header file

---
+++ printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_format - Print all format
+* @format: Format to print
+* return: If success.
+*/
+int (*print_format(const char *format))(va_list)
+{
+	print_f type[] = {
+			{"c", print_c},
+			{"s", print_s},
+			{NULL, NULL}
+	};
+}
