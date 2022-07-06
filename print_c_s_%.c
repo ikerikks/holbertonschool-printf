449a127f40132939e5b134d9580a80f482048b08
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-06 14:15:52 +0200

    functions to print character and string

--- print_c_s_%.c
+++ print_c_s_%.c
@@ -9,7 +9,7 @@ int print_c(va_list c)
 {
 	char charact = (char)va_arg(c, int);
 	
-	_putchar (charact);
+	_putchar(charact);
 	return (1);
 }
 
@@ -19,3 +19,17 @@ int print_c(va_list c)
 * Return: Success.
 */
 int print_s(va_list s)
+{
+	char *string = va_arg(s, char*);
+
+	int i = 0;
+
+	if (string == NULL)
+		string = "";
+
+	while (string[i])
+		_putchar(string[i++]);
+	
+	return (i);
+}
+
