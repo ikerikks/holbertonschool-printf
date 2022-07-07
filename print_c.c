7b767071352b6029f0358bed7535fe1b8d40b9ee
ChongLeangUENG <5129@holbertonschool.com>
2022-07-07 04:44:40 -0700

    Commit Print character and string

---
+++ print_c.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_c - Print output character
+* @c: Character to print
+* Return: Success.
+*/
+
+int print_c(va_list c)
+{
+	char charact = (char)va_arg(c, int);
+	
+	_putchar(charact);
+	return (1);
+}
