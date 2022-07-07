10595bc512310bd38dc4a330218fbc342c6d221e
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-07-07 15:28:27 +0200

    commit function _printf

--- _printf.c
+++ _printf.c
@@ -1,25 +1,78 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
 * print_format - Print all format
 * @format: Format to print
 * return: If success.
 */
-int _printf(const char *format, ...)
+int (*print_format(const char *format))(va_list)
 {
+	int j = 0;
 	print_t type[] = {
 			{"c", print_c},
 			{"s", print_s},
 			{NULL, NULL}
 	};
 
-	int j = 0;
-
-	while (j < 10)
+	for (; type[j].s != NULL; j++)
 	{
-		if (format[0] == type->s[j])
+		if (type[j].s == *format)
 			break;
-		j++;
 	}
-	return (j);
+	return (type[j].i);
+}
+
+/**
+* _printf - function that produces output of format 
+* @format: a pointer to produce
+* @...: others
+* Return: the number of characters print
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int (*f)(va_list);
+
+	unsigned int y = 0, x = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	while (format && format[y])
+	{
+		if (format[y] != '%')
+		{
+			_putchar(format[y]);
+			x++;
+			y++;
+			continue;
+		}
+		else 
+		{
+			if (format[y + 1] == '%')
+			{
+				_putchar('%');
+				x++;
+				y += 2;
+				continue;
+			}
+			else
+			{
+				f = print_format(&format[y + 1]);
+				if (f == NULL)
+					return (-1);
+				y += 2;
+				x += f(list);
+				continue;
+			}
+		}
+		y++;
+	}
+	va_end(list);
+	return (x);
 }
