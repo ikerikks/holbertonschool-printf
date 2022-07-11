b8642b6f5a65d48231cb8764d636380eb4a2f044
ChongLeangUENG <5129@holbertonschool.com>
2022-07-11 02:36:39 -0700

    Commit for task 3 print binary

--- print_b.c
+++ print_b.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_b - A function that print unsigned int argument is converted to binary
@@ -14,12 +16,12 @@ int print_b(va_list b)
 	n = va_arg(b, unsigned int);
 	m = 2147483648;
 	a[0] = n / m;
-	for (o = 0; o < 32; o++)
+	for (o = 1; o < 32; o++)
 	{
 		m /= 2;
 		a[o] = (n / m) % 2;
 	}
-	for (o = 0; p = 0; count = 0; o < 32; o++)
+	for (o = 0, p = 0, count = 0; o < 32; o++)
 	{
 		p += a[o];
 		if (p || o == 31)
